7e623313b2d0ede246ad248cf87151d9cdf0d5ef
Ni'mah Athafarisya <103431012+athafarisya@users.noreply.github.com>
2023-10-04 13:29:05 +0700

    Add files via upload

---
+++ lib/latihan4.dart
@@ -0,0 +1,22 @@
+void main() {
+  int a = 5;
+  int b = 10;
+
+  // Operasi perkalian
+  int perkalian = a * b;
+
+  // Operasi penjumlahan
+  int penjumlahan = a + b;
+
+  // Operasi pengurangan
+  int pengurangan = a - b;
+
+  // Operasi pembagian (dalam bentuk double)
+  double pembagian = a / b;
+
+  // Tampilkan hasil operasi
+  print('$a * $b = $perkalian');
+  print('$a + $b = $penjumlahan');
+  print('$a - $b = $pengurangan');
+  print('$a / $b = $pembagian');
+}
